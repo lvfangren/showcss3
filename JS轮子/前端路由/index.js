60e18866930d3cc5c57c42726dbc442a662f25b5
lvfangren <m18188267414@163.com>
2018-06-15 22:25:54 +0800

    2018/6/15

---
+++ JS轮子/前端路由/index.js
@@ -0,0 +1,10 @@
+var router=new FrontRouter();
+router.route('blue',function(){
+  document.body.style.backgroundColor='blue'
+})
+router.route('yellow',function(){
+  document.body.style.backgroundColor='yellow'
+})
+router.route('red',function(){
+  document.body.style.backgroundColor='red'
+})
---
+++ JS轮子/前端路由/router.js
@@ -0,0 +1,12 @@
+function FrontRouter(){
+  this.routes={};
+  window.addEventListener('load',this.resolve.bind(this),false)
+  window.addEventListener('hashchange',this.resolve.bind(this),false)
+}
+FrontRouter.prototype.route=function(path,calback){
+  this.routes[path]=calback||function(){};
+}
+FrontRouter.prototype.resolve=function(){
+  this.curHash=location.hash.slice(1)||'/'
+  typeof this.routes[this.curHash]==='function'&&this.routes[this.curHash]();
+}
